64e27de4bd4ab387b422b3449de2e74ac296c9a8
vinceliuice <vinceliuice@hotmail.com>
2023-04-08 22:51:38 +0800

    update

--- install.sh
+++ install.sh
@@ -17,14 +17,14 @@ OPTIONS:
 
   -l, --libadwaita        Link installed Orchis gtk-4.0 theme to config folder for all libadwaita app use Orchis theme
 
-  --tweaks                Specify versions for tweaks [solid|compact|black|primary|macos|submenu|(nord/dracula)] (Options can mix [nord and dracula can not mix use!])
+  --tweaks                Specify versions for tweaks [solid|compact|black|primary|macos|submenu|(nord/dracula)] (Options can mix)
                           1. solid:              No transparency panel variant
                           2. compact:            No floating panel variant
                           3. black:              Full black variant
                           4. primary:            Change radio icon checked color to primary theme color (Default is Green)
                           5. macos:              Change window buttons to MacOS style
                           6. submenu:            Theme sub-menus, by Default submenus contrast
-                          7. [nord|dracula]:     Nord/dracula colorscheme themes
+                          7. [nord|dracula]:     Nord/dracula colorscheme themes (nord and dracula can not mix use!)
 
   --round                 Change theme round corner border-radius [Input the px value you want] (Suggested: 2px < value < 16px)
                           1. 3px
